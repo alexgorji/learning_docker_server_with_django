612852655c4331cd2989ee30e33e1f42fec8d4fd
alexgorji <aligorji@hotmail.com>
2024-04-20 15:45:00 +0200

    7 3 backup scripts

--- 7_deploy_demo_django_project/3_git_backup/git-backup/create_backup.sh
+++ 7_deploy_demo_django_project/3_git_backup/git-backup/create_backup.sh
@@ -3,15 +3,19 @@
 
 cd "$BACKUP_REPOSITORY_NAME"
 
+git pull --rebase
 mkdir -p ./backup-data
+
+rm -rf ./backup-data/*
+
 cp -r "$BACKUP_DATA_PATH"/. ./backup-data/
 
 git add .
+
 if git status | grep "nothing to commit" >/dev/null; then
   echo "nothing to commit"
 else
   git commit -m backup
 fi
 
-git pull --rebase
 git push
--- 7_deploy_demo_django_project/3_git_backup/git-backup/load_backup.sh
+++ 7_deploy_demo_django_project/3_git_backup/git-backup/load_backup.sh
@@ -4,6 +4,7 @@
 cd "$BACKUP_REPOSITORY_NAME"
 
 git add .
+
 if git status | grep "nothing to commit" >/dev/null; then
   echo "nothing to commit"
 else
@@ -12,7 +13,10 @@ fi
 
 git pull --rebase
 
-echo copying backup-data to repository
+echo cleaning backup
+rm -rf "$BACKUP_DATA_PATH"/*
+
+echo copying from repository to backup
 
 cp -r ./backup-data/* "$BACKUP_DATA_PATH"/
 echo done
